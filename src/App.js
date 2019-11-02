87e72af1de09dbba611e92d908cdcfa105a3b6ac
Kerri-Ann <kaybee1jam@yahoo.com>
2019-11-01 23:10:58 -0400

    set up time component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import TimeBlock from './components/TimeBlock';
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <TimeBlock />
     </div>
   );
 }
---
+++ src/components/TimeBlock.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TimeBlock = () => {
+    return (
+        <div className="time-block">
+            <div className="container">
+                <div className="date">Tuesday Jan 3, 2020</div>
+                <div className="time">11:00 AM</div>
+            </div>
+        </div>
+    )
+}
+
+export default TimeBlock;
