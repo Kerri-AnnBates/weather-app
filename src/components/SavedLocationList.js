3715e8d0849ee458d1f32b0a1e3668d0fb19db84
Kerri-Ann <kaybee1jam@yahoo.com>
2020-07-28 14:31:22 -0400

    setup sass structure

--- src/components/SavedLocationList.js
+++ src/components/SavedLocationList.js
@@ -2,23 +2,23 @@ import React from 'react';
 import SavedLocationCard from './SavedLocationCard';
 
 function SavedLocationList(props) {
-    const { savedLocations } = props;
-    return (
-        <div className="saved-container" id="saved-location">
-            <div className="container">
-                <h3>Saved Locations</h3>
-                <div className="saved-wrapper flex-container">
-                    {/* Saved location cards */}
-                    {(savedLocations.length > 0) && savedLocations.map((location, index) => (
-                        <SavedLocationCard
-                            key={index}
-                            location={location}
-                        />
-                    ))}
-                </div>
-            </div>
-        </div>
-    )
+	const { savedLocations } = props;
+	return (
+		<div className="saved-container" id="saved-location">
+			<div className="container">
+				<h3>Saved Locations</h3>
+				<div className="saved-wrapper flex-container">
+					{/* Saved location cards */}
+					{(savedLocations.length > 0) && savedLocations.map((location, index) => (
+						<SavedLocationCard
+							key={index}
+							location={location}
+						/>
+					))}
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default SavedLocationList
