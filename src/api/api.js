bce76d02e428798a15ed01ad41d0726ad23adbc9
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-02-28 14:42:46 -0500

    added https to fix error

--- src/api/api.js
+++ src/api/api.js
@@ -12,7 +12,7 @@ export const getInitialLocation = () => {
 }
 
 export const getWeahter = (city) => {
-    const url = `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_WEATHER_KEY}&query=${city}`;
+    const url = `https://api.weatherstack.com/current?access_key=${process.env.REACT_APP_WEATHER_KEY}&query=${city}`;
     // const old = `https://api.weatherbit.io/v2.0/current?city=${result.city},${result.region_code}&key=${process.env.REACT_APP_WEATHER_KEY}`;
     return axios.get(url)
         .then(res => {
