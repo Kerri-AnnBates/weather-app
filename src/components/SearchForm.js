c82f0e10153bd7f5a7bbd6be8a44a3f9e622774e
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-03-03 19:54:40 -0500

    position tooltip

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -55,7 +55,7 @@ const SearchForm = () => {
 	return (
 		<div className="search-form group">
 			<span className="help-icon"><i className="far fa-question-circle"></i>
-				<p className="help-message shadow">Enter a city name to search for weather.</p>
+				<p className="help-message shadow">Enter a city name to search for weather. For a more specifi entry, entry should be in the format of city and state code, for example: "City, St" or city and country code: "Paris, Fr".</p>
 			</span>
 			<form onSubmit={handleSubmit}>
 				<label htmlFor="search"> Search location:</label>
